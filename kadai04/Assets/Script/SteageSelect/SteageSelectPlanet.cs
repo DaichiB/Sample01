585cf8a673e0e4dcc70f991bf7b02b07e4dc4c2f
馬場大地 <baba.daichi@tecotec.co.jp>
2017-04-14 20:05:25 +0900

    planetの回転プログラムの追加

---
+++ kadai04/Assets/Script/SteageSelect/SteageSelectPlanet.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SteageSelectPlanet : MonoBehaviour {
+
+    public float speed = 360.0f;//ドラッグによる回転速度
+    public float friction = 0.1f;//フリック時の減速係数
+    Vector2 rotVel;//現在の回転速度
+
+   　//ドラッグ時に移動した距離を更新
+    public void OnDrag(BaseEventData data)
+    {
+        PointerEventData pointer = data as PointerEventData;
+        rotVel.y = -pointer.delta.x * speed;
+    }
+
+    // 
+
+	// Update is called once per frame
+	void Update () {
+        //回転させる
+        this.transform.Rotate(new Vector3(0, rotVel.y, 0), Space.World);
+        //回転の減速
+        rotVel.y *= (1 - friction);
+		
+	}
+
+    
+    //後で書き直す
+    System.Action onRotateCompete;
+
+    //選択したキャラが正面を向くように回転
+    public void RorateOnSelect(Transform enemy, System.Action onRotateCompete)
+    {
+        this.onRotateCompete = onRotateCompete;
+        Vector3 from = enemy.position;
+        from.y = 0;
+        from.Normalize();
+        Vector3 to = Vector3.back;
+        Vector3 euler = Quaternion.FromToRotation(from, to).eulerAngles;
+
+        if (euler.y > 180)
+            euler.y -= 360;
+
+       /* iTween.RotateAdd(gameObject, iTween.Hash(
+            "y", euler.y,
+            "islocal", true,
+            "speed", 60.0f,
+            "oncomplete", "OnRotateComplete",
+            "oncompletetarget", gameObject,
+            "easetype", iTween.EaseType.easeInOutQuad
+            ));*/
+    }
+
+    //回転終了
+    void OnRotateComplete()
+    {
+        onRotateCompete();
+    }
+}
